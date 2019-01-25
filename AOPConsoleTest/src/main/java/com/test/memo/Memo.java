4cf573e3c7b0716912293dbe57f1fcc198150c95
user1 <user1@Suwook>
2019-01-25 11:14:54 +0900

    20190125

---
+++ AOPConsoleTest/src/main/java/com/test/memo/Memo.java
@@ -0,0 +1,38 @@
+package com.test.memo;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Memo implements Imemo {
+
+	@Override 	//주업무 - 메모 작성하기
+	public void write(String memo) {
+		System.out.println("메모 :"+memo);
+		
+	}
+
+	@Override // 주업무 - 메모 수정하기
+	public void edit(String memo) {
+		String originMemo = "JAVA";
+		System.out.println("메모 수정 전 : "+originMemo);
+		originMemo=memo;
+		System.out.println("메모 수정 후 : "+originMemo);
+	}
+
+	@Override // 주업무 - 메모 삭제하기
+	public int del(int seq) {
+		System.out.println(seq+" 번 메모삭제");
+		return seq; // 삭제되어지 글 번호 넘기기
+	}
+
+	@Override
+	public void read(int seq) throws Exception {
+		if(seq <1) {
+			throw new Exception("번호 오류"+seq);
+		}
+		System.out.println(seq+"번 메모 읽기");
+		
+	}
+
+	
+	
+}
